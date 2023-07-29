c9202495e71a3b34a91ed124b4feafe052ef73a1
KIPROTYCH <kiprotych@gmail.com>
2023-07-29 23:15:19 +0300

    Imported the Apps from React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Blog from "./Components/Blog/Blog";
+import Offers from "./Components/Offers/Offers";
+import Popular from "./Components/Popular/Popular";
+import Footer from "./Components/Footer/Footer";
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Blog/>
+    <Offers/>
+    <Popular/>
+    <Footer/>
+    </>
+  )
+};
+
+export default App;
